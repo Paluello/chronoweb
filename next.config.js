44a834a5a0475e8cf9f5a975e87b1a260006555f
Palu17 <10946432@polimi.it>
2024-12-06 22:48:40 +0100

    Update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // altre configurazioni...
+  // Uncomment the following line if you're using GitHub Pages:
+  basePath: '/cronoweb',
 }
 
 module.exports = nextConfig
