a2536f63651a0b86576e6075549352ec131f0442
Palu17 <10946432@polimi.it>
2024-12-06 22:51:31 +0100

    RESTART

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  // Uncomment the following line if you're using GitHub Pages:
-  basePath: '/cronoweb',
-}
-
-module.exports = nextConfig
